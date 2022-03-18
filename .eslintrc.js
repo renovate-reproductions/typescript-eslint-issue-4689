88052fbc75192dc63f286d062bfc6ce2a0b3ca27
Brad Zacher <brad.zacher@gmail.com>
2022-03-18 12:17:27 -0700

    simplify lint config more (#1)
    * simplify lint config more
    
    * Update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,20 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/src/configs
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   rules: {
-    // TODO: removing these two lines to remove the lint error
-    '@typescript-eslint/no-unsafe-return': 0,
-    '@typescript-eslint/no-unsafe-argument': 0,
-
     '@typescript-eslint/restrict-template-expressions': [
-      2,
-      { allowNumber: true, allowBoolean: true },
+      'error',
+      {
+        allowNumber: true,
+        allowBoolean: true,
+      },
     ],
-    '@typescript-eslint/restrict-plus-operands': 2,
+    '@typescript-eslint/restrict-plus-operands': ['error'],
+    '@typescript-eslint/no-misused-promises': ['error'],
   },
-  overrides: [
-    {
-      // files to check, so no `--ext` is required
-      files: ['**/*.ts'],
-    },
-  ],
 };
